2ce71c484f6f3fac9539daa05d4ae9b996dd81b4
Raphaelli Bellini <rafael.bellini@hotmail.com>
2020-07-14 15:21:09 -0300

    Criar corpo da pagina de Casos cadastrados

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
 import './styles.css';
 
 function Profile() {
@@ -15,6 +15,67 @@ function Profile() {
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
+
+            <h1>Casos cadastrados</h1>
+
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste:</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste:</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste:</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste:</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+            </ul>
         </div>
     )
 }
