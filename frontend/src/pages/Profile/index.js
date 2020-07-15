b6ed084e279a859b7ee4d115a4cde71ea01a48bc
Raphaelli Bellini <rafael.bellini@hotmail.com>
2020-07-15 11:23:46 -0300

    Criar função de Logout

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import logoImg from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import './styles.css';
 import api from '../../services/api';
 
 function Profile() {
     const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
     
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
@@ -36,13 +38,18 @@ function Profile() {
         }
     }
 
+    function handlelogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Logo" />
                 <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button type="button" onClick={handlelogout}>
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
