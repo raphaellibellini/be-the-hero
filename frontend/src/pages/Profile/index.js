e772055cd1cf5c0407fd142eaaddd49cb848070f
Raphaelli Bellini <rafael.bellini@hotmail.com>
2020-07-15 10:34:31 -0300

    Exibir dinamicamente o nome da ONG 'logada'

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -5,11 +5,13 @@ import { FiPower, FiTrash2 } from 'react-icons/fi';
 import './styles.css';
 
 function Profile() {
+    const ongName = localStorage.getItem('ongName');
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Logo" />
-                <span>Bem vinda, APAD</span>
+                <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
                 <button type="button">
                     <FiPower size={18} color="#e02041" />
